55cd3b0133e50fb6da3fae118686998ecd9bcf96
Nandini <nandini.kri21@gmail.com>
2026-05-28 15:39:59 +0530

    Create qt14.kt

---
+++ qt14.kt
@@ -0,0 +1,15 @@
+fun main() {
+    val n = 30
+    println("Prime numbers up to $n:")
+
+    for (num in 2..n) {
+        var isPrime = true
+        for (i in 2..num / 2) {
+            if (num % i == 0) {
+                isPrime = false
+                break
+            }
+        }
+        if (isPrime) print("$num ")
+    }
+}
