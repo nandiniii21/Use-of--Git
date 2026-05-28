dc1a13200e20607e28acc41b07b313e04b3d9653
Nandini <nandini.kri21@gmail.com>
2026-05-28 17:27:40 +0530

    Delete q7.kt

--- q7.kt
+++
@@ -1,7 +0,0 @@
-fun findLargest(a: Int, b: Int, c: Int): Int {
-    return if (a >= b && a >= c) a else if (b >= a && b >= c) b else c
-}
-
-fun main() {
-    println("Largest: ${findLargest(12, 45, 19)}")
-}
