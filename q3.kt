706db8e38db815bed45d4a8d59a23b12fd6741db
Nandini <nandini.kri21@gmail.com>
2026-05-28 17:24:05 +0530

    Sum of numbers from 1 to n

---
+++ q3.kt
@@ -0,0 +1,12 @@
+fun sumUpToN(n: Int): Int {
+    var sum = 0
+    for (i in 1..n) {
+        sum += i
+    }
+    return sum
+}
+
+fun main() {
+    val n = 10
+    println("Sum of numbers from 1 to $n is: ${sumUpToN(n)}")
+}
